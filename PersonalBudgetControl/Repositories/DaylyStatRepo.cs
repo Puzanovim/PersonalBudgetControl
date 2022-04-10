f2b0aee312ab2ed3aecf852299da41cb3f5f1b96
Puzanovim <puzanovim@yandex.ru>
2022-04-10 22:52:04 +0500

    add Repos

---
+++ PersonalBudgetControl/Repositories/DaylyStatRepo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonalBudgetControl.Data;
+
+
+namespace PersonalBudgetControl.Repositories
+{
+    class DaylyStatRepo
+    {
+        private List<Day> daylyStats;
+        public List<Day> DaylyStatsList => daylyStats;
+
+        private void UpdateDaylyStats(int day)
+        {
+            int saldo = this.daylyStats[day].Saldo;
+            int days = this.daylyStats.Count;
+            day += 1;
+            while (saldo < 0)
+            {
+                if (day == days)
+                    break;
+                int next_expense = saldo * -1;
+                this.daylyStats[day].AddExpense(next_expense);
+                Console.WriteLine("Updated expenses at: " + day);
+                saldo = this.daylyStats[day].Saldo;
+            }
+        }
+
+        public DaylyStatRepo(int days, int oneDayBudget)
+        {
+            this.daylyStats = new List<Day>();
+            for (var i = 0; i < days; i++)
+            {
+                Day new_day = new Day(i, oneDayBudget);
+                this.daylyStats.Add(new_day);
+            }
+        }
+
+        public void AddExpense(int day, int expense)
+        {
+            this.daylyStats[day].AddExpense(expense);
+            this.UpdateDaylyStats(day);
+            Console.WriteLine("Added new expense: " + expense + " to day number " + day);
+        }
+
+        public int SumExpenses()
+        {
+            int sumExpenses = 0;
+            foreach(Day day in this.daylyStats)
+            {
+                sumExpenses += day.SumExpenses;
+            }
+            return sumExpenses;
+        }
+    }
+}
---
+++ PersonalBudgetControl/Repositories/MonthlyStatRepo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonalBudgetControl.Data;
+
+
+namespace PersonalBudgetControl.Repositories
+{
+    class MonthlyStatRepo
+    {
+        private Dictionary<Month, MonthStat> monthlyStatsHistory;
+        private MonthStat currentMonthStats;
+        public Dictionary<Month, MonthStat> History => monthlyStatsHistory;
+        public MonthStat MonthStat => currentMonthStats;
+
+        public MonthlyStatRepo()
+        {
+            this.monthlyStatsHistory = new Dictionary<Month, MonthStat>();
+        }
+
+        public MonthStat CreateNewMonth(DateTime date, List<int> incomes, List<int> mandatoryExpenditures)
+        {
+            Month currentMonth = new Month(date);
+            this.currentMonthStats = new MonthStat(incomes, mandatoryExpenditures, currentMonth);
+            return this.currentMonthStats;
+        }
+
+        public Dictionary<Month, MonthStat> CreateBackup()
+        {
+            if (this.currentMonthStats != null)
+            {
+                this.monthlyStatsHistory.Add(this.currentMonthStats.Month, this.currentMonthStats);
+            }
+                return this.monthlyStatsHistory;
+        }
+
+        public void AddIncome(List<int> incomes)
+        {
+            foreach (int income in incomes)
+                this.currentMonthStats.AddIncome(income);
+        }
+
+        public void AddMandatoryExpense(List<int> mandatoryExpenses)
+        {
+            foreach (int expense in mandatoryExpenses)
+                this.currentMonthStats.AddMandatoryExpense(expense);
+        }
+
+        public void UpdateSumExpenses(int newSumExpenses)
+        {
+            this.currentMonthStats.UpdateSumExpenses(newSumExpenses);
+        }
+    }
+}
