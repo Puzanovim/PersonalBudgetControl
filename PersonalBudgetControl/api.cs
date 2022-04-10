1e6a1c392279d0dc7d569447ced1c76f87573bc6
Puzanovim <puzanovim@yandex.ru>
2022-04-10 23:17:31 +0500

    add api and cli

---
+++ PersonalBudgetControl/api.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalBudgetControl.Repositories;
+
+
+namespace PersonalBudgetControl
+{
+    class api
+    {
+        private DaylyStatRepo daylyStatRepo;
+        private MonthlyStatRepo monthlyStatRepo;
+        public api()
+        {
+            this.monthlyStatRepo = new MonthlyStatRepo();
+        }
+
+        public void CreateNewMonth(List<int> incomes, List<int> expenses)
+        {
+            DateTime today = new DateTime();
+            this.monthlyStatRepo.CreateBackup();
+            this.monthlyStatRepo.CreateNewMonth(date: today, incomes: incomes, mandatoryExpenditures: expenses);
+            int countDays = DateTime.DaysInMonth(year: today.Year, month: today.Month);
+            int oneDayBudget = this.monthlyStatRepo.MonthStat.OneDayBudget;
+            this.daylyStatRepo = new DaylyStatRepo(days: countDays, oneDayBudget: oneDayBudget);
+        }
+
+        public Dictionary<string, object> GetMonthInfo()
+        {
+            Dictionary<string, object> monthInfo = new Dictionary<string, object>()
+            {
+                {"monthName",  this.monthlyStatRepo.MonthStat.Month.MonthName},
+                {"sumIncomes",  this.monthlyStatRepo.MonthStat.SumIncomes},
+                {"sumMandatoryExpenses",  this.monthlyStatRepo.MonthStat.SumExpenses},
+                {"oneDayBudget",  this.monthlyStatRepo.MonthStat.OneDayBudget},
+                {"totalExpenses",  this.monthlyStatRepo.MonthStat.TotalExpenses},
+                {"accumulations",  this.monthlyStatRepo.MonthStat.Accumulations},
+            };
+            return monthInfo;
+        }
+
+        public void AddIncome(List<int> incomes)
+        {
+            this.monthlyStatRepo.AddIncome(incomes);
+        }
+
+        public void AddMandatoryExpenses(List<int> mandatoryExpenses)
+        {
+            this.monthlyStatRepo.AddMandatoryExpense(mandatoryExpenses);
+        }
+
+        public void AddExpense(DateTime date, int expense)
+        {
+            this.daylyStatRepo.AddExpense(day: date.Day, expense: expense);
+            int newSumExpenses = this.daylyStatRepo.SumExpenses();
+            this.monthlyStatRepo.UpdateSumExpenses(newSumExpenses);
+        }
+    }
+}
---
+++ PersonalBudgetControl/cli.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonalBudgetControl
+{
+    class cli
+    {
+        private static List<int> getValues(string countInputText, string elementInputText)
+        {
+            Console.Write(countInputText);
+            int countElements = int.Parse(Console.ReadLine());
+            List<int> elements = new List<int>();
+            for (var i = 0; i < countElements; i++)
+            {
+                Console.Write(elementInputText + i);
+                elements.Add(int.Parse(Console.ReadLine()));
+            }
+            return elements;
+        }
+
+        private static void printMonthInfo(
+            string monthName,
+            int sumIncomes,
+            int sumMandatoryExpenses,
+            int oneDayBudget,
+            int totalExpenses,
+            int accumulations
+        )
+        {
+            Console.WriteLine("---------- " + monthName + " info ----------");
+            Console.WriteLine("Ваши доходы: " + sumIncomes);
+            Console.WriteLine("Ваши обязательные расходы: " + sumMandatoryExpenses);
+            Console.WriteLine("Подсчитан бюджет на день: " + oneDayBudget);
+            Console.WriteLine("Ваши расходы за этот месяц: " + totalExpenses);
+            Console.WriteLine("Накопления за этот месяц: " + accumulations);
+            Console.WriteLine("---------- " + monthName + " info ----------");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool goodBue = false;
+            api api = new api();
+            while (true)
+            {
+                if (goodBue)
+                    break;
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "S":
+                        Console.WriteLine("Creating new month");
+                        List<int> incomes = getValues(
+                            countInputText: "Введите количество источников вашего дохода: ",
+                            elementInputText: "Введите доход №"
+                        );
+                        List<int> expenses = getValues(
+                            countInputText: "Введите количество обязательных расходов: ",
+                            elementInputText: "Введите обязательный расход №"
+                        );
+                        api.CreateNewMonth(incomes, expenses);
+                        Dictionary<string, object> monthInfo = api.GetMonthInfo();
+                        printMonthInfo(
+                            monthName: (string) monthInfo["monthName"],
+                            sumIncomes: (int) monthInfo["sumIncomes"],
+                            sumMandatoryExpenses: (int)monthInfo["sumMandatoryExpenses"],
+                            oneDayBudget: (int)monthInfo["oneDayBudget"],
+                            totalExpenses: (int)monthInfo["totalExpenses"],
+                            accumulations: (int)monthInfo["accumulations"]
+                        );
+                        break;
+                    case "I":
+                        Console.WriteLine("Adding income");
+                        break;
+                    case "M":
+                        Console.WriteLine("Adding mandatoryexpenses");
+                        break;
+                    case "E":
+                        Console.WriteLine("Adding expense");
+                        break;
+                    case "G":
+                        Console.WriteLine("Good bue!");
+                        goodBue = true;
+                        break;
+                }                    
+            }
+        }
+    }
+}
